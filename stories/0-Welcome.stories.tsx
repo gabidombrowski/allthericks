7c421be72226f3028d4e078258a91d1264f9e961
Gabi Dombrowski <gabdombrowski@gmail.com>
2019-11-26 22:49:43 -0600

    Initial setup

---
+++ stories/0-Welcome.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf("Welcome", module)
+  .add("with text", () => (
+    <div>Hello</div>
+  ))
---
+++ stories/1-Button.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'
+
+storiesOf("Button", module)
+  .add("with some emoji", () => (
+    <div>TEST</div>
+  ));
