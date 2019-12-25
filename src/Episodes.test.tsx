cec5d19f7c91a7d159a7bca9bee38439ffa4f12e
Gabi Dombrowski <gabdombrowski@gmail.com>
2019-12-25 14:10:33 -0600

    Add wait-for-expect

--- src/Episodes.test.tsx
+++ src/Episodes.test.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { create, act } from 'react-test-renderer'
-import wait from 'waait'
-import waitForExpect from 'wait-for-expect'
-import Episodes from './Episodes'
-import AutoMockedProviderMerged from 'utils/AutoMockedProviderMerged'
+import React from "react";
+import { create, act } from "react-test-renderer";
+import wait from "waait";
+import waitForExpect from "wait-for-expect";
+import Episodes from "./Episodes";
+import AutoMockedProviderMerged from "utils/AutoMockedProviderMerged";
 
-it('renders', async () => {
+it("renders", async () => {
   const component = create(
     <AutoMockedProviderMerged>
       <Episodes />
-    </AutoMockedProviderMerged>,
-  )
+    </AutoMockedProviderMerged>
+  );
 
-  await act(() => wait(0))
+  await act(() => wait(0));
 
   await waitForExpect(() => {
-    expect(component).toMatchSnapshot()
-  })
-})
+    expect(component).toMatchSnapshot();
+  });
+});
